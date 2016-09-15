c7e6089a0eefa372183f297ee8b49f5e4694cfab
jennebumke <jennebumke@live.nl>
2016-09-15 12:56:58 +0200

    Functions, costructors and drivers set for spots

--- discLight.cs
+++ discLight.cs
@@ -3,11 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dmx512UsbRs485;
 
 namespace midiLightShow
 {
     class discLight
     {
+        Dmx512UsbRs485Driver driver = new Dmx512UsbRs485Driver();
+
+        public string comPort = "";
+
+        public discLight(string comPort)
+        {
+            this.comPort = comPort;
+            this.driver.DmxToDefault(this.comPort);
+        }
+
         //Effect functions
+        public void controls(String function)
+        {
+            byte b = 0;
+
+            switch(function)
+            {
+                case "off":
+                    b = 1;
+                    break;
+
+                case "on":
+                    b = 66;
+                    break;
+
+                case "auto":
+                    b = 255;
+                    break;
+            }
+
+            if(b > 0)
+            {
+                this.driver.DmxLoadBuffer(1, 0, 8);
+                this.driver.DmxSendCommand(1);
+            }
+            else
+            {
+                Console.WriteLine("Invalid function \"" + function + "\" in control(" + function + ")");
+            }
+            
+        }
+
+        public void motor (String function, Int32 speed)
+        {
+            byte b = 0;
+
+            switch(function)
+            {
+                case "counterClockWise":
+                    b = 1;
+                    break;
+
+                case "clockWise":
+                    b = 86;
+                    break;
+
+                case "shake":
+                    b = 171;
+                    break;
+            }
+
+            if(b > 0 && speed < 85)
+            {
+                b = Convert.ToByte(b + speed);
+            }
+            else if(speed > 84)
+            {
+                Console.WriteLine("Error: Motor speed limit exceeded! [Speed > 84 in motor("+function + speed.ToString()+")]");
+            }
+            else
+            {
+                Console.WriteLine("Invalid function \"" + function + "\" in motor(" + function + speed.ToString() + ")");
+            }
+        }
+
     }
 }
--- lazer.cs
+++ lazer.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dmx512UsbRs485
 
 namespace midiLightShow
 {
     class lazer
     {
+        Dmx512UsbRs485Driver driver = new Dmx512UsbRs485Driver();
+
+        public string comPort = "";
+
+        public lazer(string comPort)
+        {
+            this.comPort = comPort;
+            this.driver.DmxToDefault(this.comPort);
+        }
+
         //Fire up your lazor
     }
 }
