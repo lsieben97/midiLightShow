e7e08d83828a9d6aa64873361983b7a525dad5a2
jennebumke <jennebumke@live.nl>
2016-09-29 10:40:20 +0200

    toggle Function added

--- lazer.cs
+++ lazer.cs
@@ -11,22 +11,32 @@ namespace midiLightShow
     {
         Dmx512UsbRs485Driver driver = new Dmx512UsbRs485Driver();
 
+        bool lamp = false;
+
+        public lazer() { }
         public lazer(string comPort)
         {
             this.comPort = comPort;
             this.driver.DmxToDefault(this.comPort);
+            this.driver.DmxLoadBuffer(1, 255, 10);
+            this.driver.DmxSendCommand(1);
         }
 
         //Fire up your lazor
         public void autoRun()
         {
-            this.driver.DmxLoadBuffer(1,0,8);
+            this.driver.DmxLoadBuffer(1,0,10);
             this.driver.DmxSendCommand(1);
         }
 
-        public void func_Off()
+        public void func_toggleLamp()
         {
+            if(lamp)
+                lamp = false;
+            else
+                lamp = true;
 
+            this.driver.DmxLoadBuffer(3, Convert.ToByte(Convert.ToByte(lamp) * 25), 10);
         }
 
 
