70475f0343ccf02295d6f17e291c6500e24df17a
jennebumke <jennebumke@live.nl>
2016-09-29 12:20:19 +0200

    More lazer options!

--- lazer.cs
+++ lazer.cs
@@ -12,6 +12,7 @@ namespace midiLightShow
         Dmx512UsbRs485Driver driver = new Dmx512UsbRs485Driver();
 
         bool lamp = false;
+        bool color = false;
 
         public lazer() { }
         public lazer(string comPort)
@@ -37,8 +38,50 @@ namespace midiLightShow
                 lamp = true;
 
             this.driver.DmxLoadBuffer(3, Convert.ToByte(Convert.ToByte(lamp) * 25), 10);
+            this.driver.DmxSendCommand(1);
         }
 
+        public void func_color()
+        {
+            if (color)
+                color = false;
+            else
+                color = true;
+
+            this.driver.DmxLoadBuffer(3, Convert.ToByte(Convert.ToByte(color) * 25 + 50), 10);
+            this.driver.DmxSendCommand(1);
+        }
+
+        public void func_colorCycle()
+        {
+            this.driver.DmxLoadBuffer(3, 125, 10);
+            this.driver.DmxSendCommand(1);
+        }
+
+        public void func_frequency(byte speed)
+        {
+            this.driver.DmxLoadBuffer(4, speed, 10);
+            this.driver.DmxSendCommand(1);
+        }
+
+        public void func_zoom(byte magnify)
+        {
+            this.driver.DmxLoadBuffer(5, (byte)(magnify / 2), 10);
+            this.driver.DmxSendCommand(1);
+        }
+
+        public void func_autoZoom(byte speed)
+        {
+            this.driver.DmxLoadBuffer(5, (byte)(speed / 255 * 45), 10);
+            this.driver.DmxSendCommand(1);
+        }
+
+        public void func_xy(byte x, byte y)
+        {
+            this.driver.DmxLoadBuffer(6, (byte)(x / 2), 10);
+            this.driver.DmxLoadBuffer(7, (byte)(y / 2), 10);
+            this.driver.DmxSendCommand(2);
+        }
 
     }
 }
