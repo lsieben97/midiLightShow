0ba63b4296209d4d8d645f68e2fbea03d86de770
jennebumke <jennebumke@live.nl>
2016-10-05 12:25:28 +0200

    360 no scope cleanup

--- 360SpotLight.cs
+++ 360SpotLight.cs
@@ -6,11 +6,27 @@ using System.Threading.Tasks;
 
 namespace midiLightShow
 {
+    /// <summary>
+    /// Moving head class. Functions for the Moving head DMX light
+    /// Inherited from dmxLight
+    /// </summary>
     class _360SpotLight : dmxLight
     {
+        #region Global variables
+        /// <summary>
+        /// A dictionary for all the gobo patterns
+        /// </summary>
         private Dictionary<string, byte> goboMap = new Dictionary<string, byte>();
-
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Empty Constructor
+        /// </summary>
         public _360SpotLight() { }
+        /// <summary>
+        /// Constructor for moving head class
+        /// </summary>
+        /// <param name="comPort">Connection port</param>
         public _360SpotLight(string comPort)
         {
             this.comPort = comPort;
@@ -25,18 +41,23 @@ namespace midiLightShow
             this.goboMap.Add("tangledLines", 104);
             this.goboMap.Add("bubbles", 117);
         }
-
-
+        #endregion
+        #region Execute method
+        /// <summary>
+        /// execute function method override from parent dmxLight
+        /// </summary>
+        /// <param name="function">Function given from track</param>
+        /// <param name="parameters">Paramaters given with the function</param>
+        /// <param name="execute">Indicator variable for executing the function</param>
+        /// <returns>Parameter validation</returns>
         public override bool executeFunction(string function, Dictionary<string, string> parameters, bool execute = false)
         {
-            List<string> strings = new List<string>();
-            List<int> ints = new List<int>();
-            List<bool> bools = new List<bool>();
-
+            //parameter variables that occur in several functions
             uint rotation = 0;
             uint value = 0;
             byte speed = 0;
 
+            //execute function switch case
             switch (function)
             {
                 case "setPan":
@@ -145,138 +166,205 @@ namespace midiLightShow
             }
             return false;
         }
+        #endregion
+        #region Functions
 
-        //Functions below
-
+        /// <summary>
+        /// Sets the horizontal rotation of the moving head
+        /// </summary>
+        /// <param name="rotation">Rotation degree variable</param>
         [ParameterDataAtribute(parameterDescription = new string[] { "Rotation angle" })]
         [MethodDescriptionAtribute(methodDescription = "Rotate Horizontally")]
         public void func_setPan(uint rotation)
         {
+            //Escape too big degree numbers to readable ones
             while(rotation > 540)
             {
                 rotation = rotation - 360;
             }
 
+            //Load and send the value to the moving head
             this.driver.DmxLoadBuffer(1,(byte)(rotation / 540 * 255),13);
             this.driver.DmxSendCommand(1);
         }
 
+        /// <summary>
+        /// Sets the vertical rotation of the moving head
+        /// </summary>
+        /// <param name="rotation">Rotation degree variable</param>
         [ParameterDataAtribute(parameterDescription = new string[] { "Rotation angle" })]
         [MethodDescriptionAtribute(methodDescription = "Rotate Vertically")]
         public void func_setTilt(uint rotation)
         {
-
+            //Escape too big degree numbers to readable ones
             while(rotation > 180)
             {
                 rotation = rotation - 180;
             }
 
+            //Load and send the value to the moving head
             this.driver.DmxLoadBuffer(3, (byte)(rotation / 180 * 255), 13);
             this.driver.DmxSendCommand(1);
         }
 
+        /// <summary>
+        /// Sets the shutter open or closed
+        /// </summary>
+        /// <param name="shutter">Variables to open or close the shutter</param>
         [ParameterDataAtribute(parameterDescription = new string[] { "Shutter closed" })]
         [MethodDescriptionAtribute(methodDescription = "Open or close the shutter")]
         public void func_shutter(bool shutter)
         {
+            //set default value shutter open
             byte s = 255;
 
+            //checks on behalf of the parameter if the value of the shutter should be closed
             if(shutter)
             {
                 s = 0;
             }
 
+            //Loads and sends the value to the moving head
             this.driver.DmxLoadBuffer(6, s, 13);
             this.driver.DmxSendCommand(1);
         }
 
+        /// <summary>
+        /// Sets the value of how bright the light should shine in percentage
+        /// </summary>
+        /// <param name="value">Percentage value to set the dimmer brightness</param>
         [ParameterDataAtribute(parameterDescription = new string[] { "Dimmer value" })]
         [MethodDescriptionAtribute(methodDescription = "Turn on the dimmer")]
         public void func_dimmer(uint value)
         {
+            //Escape too big number to readable ones
             while(value > 100)
             {
                 value = value - 100;
             }
 
+            //Loads and sends the value to the moving head
             this.driver.DmxLoadBuffer(6, (byte)(value / 100 * 127), 13);
             this.driver.DmxSendCommand(1);
         }
 
+        /// <summary>
+        /// Turns on the stroboscope
+        /// </summary>
+        /// <param name="value">Sets the interval of the stroboscope</param>
         [ParameterDataAtribute(parameterDescription = new string[] { "Stroboscope value" })]
         [MethodDescriptionAtribute(methodDescription = "Turn on the stroboscope")]
         public void func_strobo(uint value)
         {
+            //Escape too big numbers to readable ones
             while (value > 100)
             {
                 value = value - 100;
             }
 
+            //Loads and sends the value to the moving head
             this.driver.DmxLoadBuffer(6, (byte)(value / 100 * 105), 13);
             this.driver.DmxSendCommand(1);
         }
 
+        /// <summary>
+        /// Sets the color of the moving head
+        /// </summary>
+        /// <param name="r">The red light value</param>
+        /// <param name="g">The green light value</param>
+        /// <param name="b">The blue light value</param>
         [ParameterDataAtribute(parameterDescription = new string[] { "Red", "Green" , "Blue"})]
         [MethodDescriptionAtribute(methodDescription = "Set rgb values")]
         public void func_rgb(byte r = 0, byte g = 0, byte b = 0)
         {
+            //Loads and sends the values to the moving head
             this.driver.DmxLoadBuffer(7, r, 13);
             this.driver.DmxLoadBuffer(8, g, 13);
             this.driver.DmxLoadBuffer(9, b, 13);
             this.driver.DmxSendCommand(3);
         }
 
+        /// <summary>
+        /// Sets the movement speed
+        /// </summary>
+        /// <param name="mSpeed">Movement speed value</param>
         [ParameterDataAtribute(parameterDescription = new string[] { "Moving speed" })]
         [MethodDescriptionAtribute(methodDescription = "Set movement speed")]
         public void func_setSpeed(byte mSpeed)
         {
+            //Loads and sends the value to the moving head
             this.driver.DmxLoadBuffer(5, mSpeed, 13);
             this.driver.DmxSendCommand(1);
         }
 
-        [ParameterDataAtribute(parameterDescription = new string[] { "Direction of color order", "Speed of changing colors" })]
+        /// <summary>
+        /// Turns on a rainbow effect at a certain speed
+        /// </summary>
+        /// <param name="direction">Direction of color order</param>
+        /// <param name="speed">Speed of color change</param>
+        [ParameterDataAtribute(parameterDescription = new string[] { "Direction of color order", "Speed of color change" })]
         [MethodDescriptionAtribute(methodDescription = "Turn on a rainbow effect")]
         public void func_rainBow(bool direction,byte speed)
         {
-            byte d = 255;
+            //Set default value clockwise
+            byte d = 232;
 
+            //Checks wether the value needs to be counter clockwise
             if(direction)
             {
-                d = 232;
+                d = 255;
             }
 
+            //Loads an sends the values to the moving head
             this.driver.DmxLoadBuffer(10, d, 13);
             this.driver.DmxLoadBuffer(11, (byte)(255 / speed + 1), 13);
+            this.driver.DmxSendCommand(2);
         }
 
+        /// <summary>
+        /// Sets a gobo pattern
+        /// </summary>
+        /// <param name="gobo">Gobo name variable</param>
         [ParameterDataAtribute(parameterDescription = new string[] { "Gobo:\n- None\n- bananaSpiral\n- eclipseSun\n- flower\n- portal\n- heatWaves\n- virus\n- sun\n- tangledLines\n- bubbles" })]
         [MethodDescriptionAtribute(methodDescription = "Set a gobo")]
         public void func_gobo(string gobo)
         {
+            //Set default gobo
             byte val = 0;
+
+            //Selects gobo if given parameter is a valid gobo name
             if(this.goboMap.ContainsKey(gobo))
             {
                 val = this.goboMap[gobo];
             }
 
+            //Loads and sends the value to the moving head
             this.driver.DmxLoadBuffer(13, val, 13);
             this.driver.DmxSendCommand(1);
         }
 
+        /// <summary>
+        /// Sets the direction and speed of gobo patterns
+        /// </summary>
+        /// <param name="clockWise">Sets the direction</param>
+        /// <param name="speed"></param>
         [ParameterDataAtribute(parameterDescription = new string[] { "Direction of gobo order", "Speed of scrolling" })]
         [MethodDescriptionAtribute(methodDescription = "Turn on a scrollthrough the gobos")]
         public void func_goboScroll(bool clockWise, byte speed)
         {
+            //Sets default value + the speed
             byte val = Convert.ToByte(128+speed/2);
 
+            //Checks wether the rotation is clockWise or not
             if(!clockWise)
             {
                 val = Convert.ToByte(val + 128);
             }
 
+            //Loads and sends the value to the moving head
             this.driver.DmxLoadBuffer(13, val, 13);
             this.driver.DmxSendCommand(1);
         }
-
+        #endregion
     }
 }
